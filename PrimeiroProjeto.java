103356aefa719d3e1f25f01acd515a4946ee6a64
FabricioIkehara <fah.hiroshi@gmail.com>
2023-01-26 10:44:17 -0200

    PrimeiroProjeto

---
+++ PrimeiroProjeto.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeiroprojeto;
+
+/**
+ *
+ * @author Fabrício
+ */
+public class PrimeiroProjeto {
+
+    public static void main(String[] args) {
+        //imprimir ola mundo
+        //imprimir o numero 522
+        //imprimir uma linha em branco
+        //imprimir mais um texto
+        System.out.println("Olá mundo!"); 
+        System.out.println("522");
+        System.out.println();
+        System.out.println("Seja bem vindo a lista de exercicios!!");
+        
+    }
+}
